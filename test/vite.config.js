4f1c0fa4c7939adf51d63f41c9bf6b0f326f440d
null <null>
2023-10-11 13:13:56 -0500

    plain js

--- test/vite.config.js
+++ test/vite.config.js
@@ -3,5 +3,11 @@ import directoryIndex from "vite-plugin-directory-index";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    fs: {
+      allow: ["not-index.html", "folder", ".env.example"],
+      deny: ["exclude-me.txt"]
+    }
+  },
   plugins: [directoryIndex()],
 });
